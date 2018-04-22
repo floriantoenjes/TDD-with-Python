7e6b59fd9015353e28ca54c0487a3e16dbf36fa8
Florian Tönjes <skatepainter@yahoo.de>
2018-04-22 09:33:17 +0200

    Changed linux username in nginx.template.conf.

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,6 +1,6 @@
 import random
 from fabric.contrib.files import append, exists
-from fabric.api import  cd, env, local, run
+from fabric.api import cd, env, local, run
 
 REPO_URL = 'https://github.com/floriantoenjes/TDD-with-Python.git'
 
